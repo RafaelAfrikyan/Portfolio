3f339fbc550db31d5ab89887a07fcd16cd7a9bcd
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2022-04-29 22:28:45 +0400

    feat: add cards

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,26 +10,71 @@ export default function About() {
             alt=""
           />
           <div className="wrapText">
-            <p>Name: Rafael Afrikyan</p>
-            <p>Profile: Frontend developer</p>
+            <h2> Rafael Afrikyan</h2>
+            <p> Frontend developer</p>
           </div>
         </div>
-        <h1>Skills</h1>
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>JavaScript</p>
-        <p>ReactJS</p>
+        <div className="aboutSkills">
+          <h1>Skills</h1>
+          <img
+            className="skillimg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png"
+            alt=""
+          />
+
+          <img
+            className="skillimg"
+            src="https://icon-library.com/images/css-icon-png/css-icon-png-0.jpg"
+            alt=""
+          />
+          <img
+            className="skillimg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png"
+            alt=""
+          />
+          <img
+            className="skillimg"
+            src="https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png"
+            alt=""
+          />
+        </div>
       </div>
+
       <div className="AboutLeftside">
-        <h1>About me</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-          atque incidunt cupiditate id expedita sint sunt vero numquam corporis
-          laboriosam, fugiat a nemo est culpa, accusantium temporibus. Error
-          iusto sequi ab voluptatum assumenda laborum consequuntur aperiam
-          dignissimos soluta officia quidem, a harum suscipit velit modi. Omnis,
-          accusamus! Accusantium, illo commodi.x``
-        </p>
+        <div>
+          <h1>About me</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            voluptas sunt repellat eos odio culpa in, quam nemo velit impedit
+            sequi nobis numquam suscipit. Quasi dolores tenetur labore cum
+            incidunt animi? Quia nisi quod illo? Possimus facilis sequi optio
+            corrupti omnis voluptas necessitatibus architecto obcaecati, nam
+            commodi id officia debitis, ullam eius! Aliquid veritatis, suscipit
+            veniam amet esse voluptatibus temporibus quo ea? Dignissimos velit
+            laudantium id minus rem. Fugiat possimus aperiam fugit. Harum aut,
+            inventore aliquid, pariatur optio rerum cupiditate deserunt mollitia
+            ex reprehenderit tenetur, quibusdam atque aliquam. Placeat aperiam
+            provident officia quaerat facere obcaecati assumenda perspiciatis?
+            Sed, consequatur in. Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Labore perferendis impedit eum dolorem animi
+            commodi laborum recusandae quaerat neque repudiandae? Amet ipsa
+            fugiat est quis nobis perferendis unde corrupti reiciendis quidem
+            dolor. Voluptatem eius amet error cumque. Corporis deleniti sunt nam
+            ea, voluptas fuga neque officiis quis earum libero officia, dolor
+            harum atque! Labore, doloremque quos sunt expedita eaque aut illum
+            explicabo officia mollitia consequuntur molestias voluptates, iure
+            neque veniam itaque! Vero, provident cumque odio amet illum, error
+            porro officiis suscipit, sit dolore illo vel adipisci maiores
+            temporibus consectetur culpa id eaque! Ut officiis nostrum amet
+            aliquid dolorum accusantium optio? Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Ducimus atque incidunt cupiditate id
+            expedita sint sunt vero numquam corporis laboriosam, fugiat a nemo
+            est culpa, accusantium temporibus. Error iusto sequi ab voluptatum
+            assumenda laborum consequuntur aperiam dignissimos soluta officia
+            quidem, a harum suscipit velit modi. Omnis, accusamus! Accusantium,
+            illo commodi.x``
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import ImgMediaCard from "./Card";
+import timer from "../assets/timer.png";
+import calculator from "../assets/calculator.png";
+import trello from "../assets/trello.png";
+import shop from "../assets/shop.png";
+import todo from '../assets/TODO.png'
 
 export default function Portfolio() {
-  return <div>Here will be Portfolio page</div>;
+  return (
+    <div className="portfolio">
+      <ImgMediaCard
+        title={"Sea Battle"}
+        photo={
+          "https://raw.githubusercontent.com/RafaelAfrikyan/seaBattle/master/src/assets/Screenshot%202022-04-20%20181751.png"
+        }
+      />
+      <ImgMediaCard title={"Calculator"} photo={calculator} />
+      <ImgMediaCard title={"Timer"} photo={timer} />
+      <ImgMediaCard title={"Trello"} photo={trello} />
+      <ImgMediaCard title={"Online shop"} photo={shop} />
+      <ImgMediaCard title={"ToDo"} photo={todo} />
+    </div>
+  );
 }
